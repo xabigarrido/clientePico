099fb3a45959f47dc576723f72231861870591fa
xabi <xabigarrido@gmail.com>
2022-10-07 19:07:17 +0200

    up

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 
-export const API = "http://192.168.0.14:4000/api";
-export const URL = "http://192.168.0.14:4000/";
-// export const API = "https://lapiconera.herokuapp.com/api"
-// export const URL = "https://lapiconera.herokuapp.com/";
+// export const API = "http://192.168.0.14:4000/api";
+// export const URL = "http://192.168.0.14:4000/";
+export const API = "https://lapiconera.herokuapp.com/api"
+export const URL = "https://lapiconera.herokuapp.com/";
 
 
 export const changeInfoUser = async (id, action) => {
