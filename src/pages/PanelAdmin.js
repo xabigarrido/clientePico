bee7d6001349ae6d91eff6afbf2fc737668998df
xabi <xabigarrido@gmail.com>
2022-10-20 08:36:18 +0200

    subida

--- src/pages/PanelAdmin.js
+++ src/pages/PanelAdmin.js
@@ -390,6 +390,7 @@ export default function PanelAdmin() {
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
+                <option value="2025">2025</option>
               </select>
             </div>
             <div className={`modal-body bg-${user.mode} text-${user.mode == 'ligth' ? 'dark' : 'white'}`}>
