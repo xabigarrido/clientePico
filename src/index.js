0479e7a4ede804cdf7a101961d5ffecb0691ed73
xabi <xabigarrido@gmail.com>
2022-10-24 06:20:53 +0200

    ss

--- src/index.js
+++ src/index.js
@@ -30,4 +30,4 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))s
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.1ws2ly/CRA-vitals
